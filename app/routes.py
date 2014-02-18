9ba08d62cc5efd1a5c4a7f07960efb050dfeb682
Austin Longo <alongo117@gmail.com>
2014-02-17 19:33:38 -0700

    view update

--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,32 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+charizard = {'name': 'Charizard', 'number': '006', 'type': 'Fire/Flying', 'height': "5\' 7\" (1.70m)", 'weight': '199.5 lbs (90.5 kg)', 'picture': 'http://img.pokemondb.net/artwork/charizard.jpg'}
+bulbasor = {'name': 'Bulbasor', 'number': '001', 'type': 'Grass/Poison', 'height': "2\' 4\" (0.71m)", 'weight': '15.2 lbs (6.9 kg)', 'picture': 'http://img.pokemondb.net/artwork/bulbasaur.jpg'}
+
+poke_dict = {'1': bulbasor, '6': charizard}
+
+
 @app.route('/')
 def home():
     return render_template('home.html')
+
 @app.route('/search')
+def search():    
     return render_template('search.html')
 
 @app.route('/view')
 def view():
     return render_template('view.html')
+
+@app.route('/view/<id>')
+def view_poke(id=None):
+    if(id == '6'):
+    	return render_template('view.html', id=poke_dict['6'])
+    elif(id == '1'):
+    	return render_template('view.html', id=poke_dict['1'])
+    else:
+    	return render_template('view.html', id=None)
 
 if __name__ == '__main__':
     app.run(debug=True)
